66505adf31548829230cb8e5072fb3fd88aff1c7
Igreeda <82223725+Igggor@users.noreply.github.com>
2024-03-30 13:10:56 +0300

    task3 minor fix

--- task3.py
+++ task3.py
@@ -1,24 +1,33 @@
-with open("tasks/magical.csv", 'r', encoding="utf-8") as file:
-    lines = []
-    header = file.readline()
-    for line in file:
-        data = line.replace("\n", '').split(",")
-        lines.append(data)
-    print(lines)
-    command = input()
-    while command != "стоп":
-        data = []
-        out = None
-        maxi = -(10**10)
-        for i in range(len(lines)):
-            if lines[i][-1] == command:
-                count = int(lines[i][1])
-                if count > maxi:
-                    maxi = count
-                out = lines[i][0]
-
-        if out is None:
-            print("Такую траву мы не используем")
-        else:
-            print(f"{command} используется в {out}, его количество составялет {maxi}")
+def search_in_data_base():
+    """
+    Функция для работы с базой данных трав. Результат выполнения выводится в консоль
+    :return:
+    """
+    with open("tasks/magical.csv", 'r', encoding="utf-8") as file:
+        lines = []
+        header = file.readline()
+        for line in file:
+            data = line.replace("\n", '').split(",")
+            lines.append(data)
+        print(lines)
         command = input()
+        while command != "стоп":
+            data = []
+            out = None
+            maxi = -(10**10)
+            for i in range(len(lines)):
+                if lines[i][-1] == command:
+                    count = int(lines[i][1])
+                    if count > maxi:
+                        maxi = count
+                    out = lines[i][0]
+
+            if out is None:
+                print("Такую траву мы не используем")
+            else:
+                print(f"{command} используется в {out}, его количество составялет {maxi}")
+            command = input()
+
+
+if __name__ == '__main__':
+    search_in_data_base()
